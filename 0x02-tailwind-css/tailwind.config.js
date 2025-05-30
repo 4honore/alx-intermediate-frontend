d6ca337c453d57c09dfbf9ec484411be4b261651
4honore <ishimwehonore450@gmail.com>
2025-05-30 14:20:06 +0200

    Master Tailwind CSS Configuration Build Responsive Layouts Combine CSS Grid and Flexbox Design Aesthetically Pleasing Components Optimize for Professional Development

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // This is actually correct for your structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
